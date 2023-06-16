675ce2931a6691eb780103ac869078c91fa10a14
Umid Olimzhanov <uolimzhanov@gmail.com>
2023-06-16 23:46:39 +0500

    updated seed content

--- Data/Repository/Seed.cs
+++ Data/Repository/Seed.cs
@@ -45,32 +45,32 @@ public static class Seed
         
         var user2 = new User
         {
-            CardCredentials = "1234 5678 9898 7654", 
-            FullName = "Olimzhanov Umid", 
-            Email = "uolimzhanov@gmail.com",
+            CardCredentials = "3421 5678 9898 7654", 
+            FullName = "Mirgulom Mirsolikhov", 
+            Email = "mirsolikhov@gmail.com",
             PhoneNumber = "+998901234567"
         };
 
         var cinemaHall2 = new CinemaHall
         {
-            NumberOfSeats = 2,
-            Class = "Premium"
+            NumberOfSeats = 5,
+            Class = "Ultra"
         };
 
         var session2 = new Session
         {
-            CinemaHall = cinemaHall1,
-            Date = "12.12.2012",
-            Time = "21:12",
-            MovieName = "A Knives Out Mystery: Glass Onion",
-            SessionDuration = 132
+            CinemaHall = cinemaHall2,
+            Date = "11.11.2013",
+            Time = "11:11",
+            MovieName = "Astral",
+            SessionDuration = 112
         };
 
         var order2 = new Order
         {
-            Price = 12000,
-            Session = session1,
-            User = user1,
+            Price = 14000,
+            Session = session2,
+            User = user2,
             PaymentStatus = true
         };
 
@@ -80,32 +80,32 @@ public static class Seed
         
         var user3 = new User
         {
-            CardCredentials = "1234 5678 9898 7654", 
-            FullName = "Olimzhanov Umid", 
-            Email = "uolimzhanov@gmail.com",
+            CardCredentials = "1234 8765 9898 7654", 
+            FullName = "Nuraliev Umid", 
+            Email = "nuraliev@gmail.com",
             PhoneNumber = "+998901234567"
         };
 
         var cinemaHall3 = new CinemaHall
         {
             NumberOfSeats = 2,
-            Class = "Premium"
+            Class = "S"
         };
 
         var session3 = new Session
         {
-            CinemaHall = cinemaHall1,
+            CinemaHall = cinemaHall3,
             Date = "12.12.2012",
             Time = "21:12",
-            MovieName = "A Knives Out Mystery: Glass Onion",
+            MovieName = "The boys: The movie",
             SessionDuration = 132
         };
 
         var order3 = new Order
         {
-            Price = 12000,
-            Session = session1,
-            User = user1,
+            Price = 22000,
+            Session = session3,
+            User = user3,
             PaymentStatus = true
         };
 
@@ -116,32 +116,32 @@ public static class Seed
         var user4 = new User
         {
             CardCredentials = "1234 5678 9898 7654", 
-            FullName = "Olimzhanov Umid", 
-            Email = "uolimzhanov@gmail.com",
+            FullName = "Pulatxojaev Akbarxoja", 
+            Email = "pulatxojaev@gmail.com",
             PhoneNumber = "+998901234567"
         };
 
         var cinemaHall4 = new CinemaHall
         {
             NumberOfSeats = 2,
-            Class = "Premium"
+            Class = "BussinesMas"
         };
 
         var session4 = new Session
         {
-            CinemaHall = cinemaHall1,
-            Date = "12.12.2012",
+            CinemaHall = cinemaHall4,
+            Date = "12.12.2014",
             Time = "21:12",
-            MovieName = "A Knives Out Mystery: Glass Onion",
-            SessionDuration = 132
+            MovieName = "Free Guy",
+            SessionDuration = 142
         };
 
         var order4 = new Order
         {
-            Price = 12000,
-            Session = session1,
-            User = user1,
-            PaymentStatus = true
+            Price = 300,
+            Session = session4,
+            User = user4,
+            PaymentStatus = false
         };
 
         #endregion
@@ -150,33 +150,33 @@ public static class Seed
 
         var user5 = new User
         {
-            CardCredentials = "1234 5678 9898 7654", 
-            FullName = "Olimzhanov Umid", 
-            Email = "uolimzhanov@gmail.com",
+            CardCredentials = "1234 5678 8989 7654", 
+            FullName = "Alimov Shukrullo", 
+            Email = "alimov@gmail.com",
             PhoneNumber = "+998901234567"
         };
 
         var cinemaHall5 = new CinemaHall
         {
             NumberOfSeats = 2,
-            Class = "Premium"
+            Class = "Est deshevle"
         };
 
         var session5 = new Session
         {
-            CinemaHall = cinemaHall1,
+            CinemaHall = cinemaHall5,
             Date = "12.12.2012",
             Time = "21:12",
-            MovieName = "A Knives Out Mystery: Glass Onion",
+            MovieName = "Fast & Furious",
             SessionDuration = 132
         };
 
         var order5 = new Order
         {
-            Price = 12000,
-            Session = session1,
-            User = user1,
-            PaymentStatus = true
+            Price = 100,
+            Session = session5,
+            User = user5,
+            PaymentStatus = false
         };
         
         #endregion
@@ -199,7 +199,7 @@ public static class Seed
 
         var session6 = new Session
         {
-            CinemaHall = cinemaHall1,
+            CinemaHall = cinemaHall6,
             Date = "12.12.2012",
             Time = "21:12",
             MovieName = "A Knives Out Mystery: Glass Onion",
@@ -209,43 +209,43 @@ public static class Seed
         var order6 = new Order
         {
             Price = 12000,
-            Session = session1,
-            User = user1,
+            Session = session6,
+            User = user6,
             PaymentStatus = true
         };
 
         #endregion
-        
+
         #region 7
         
         var user7 = new User
         {
-            CardCredentials = "1234 5678 9898 7654", 
-            FullName = "Olimzhanov Umid", 
-            Email = "uolimzhanov@gmail.com",
+            CardCredentials = "3421 5678 9898 7654", 
+            FullName = "Mirgulom Mirsolikhov", 
+            Email = "mirsolikhov@gmail.com",
             PhoneNumber = "+998901234567"
         };
 
         var cinemaHall7 = new CinemaHall
         {
-            NumberOfSeats = 2,
-            Class = "Premium"
+            NumberOfSeats = 5,
+            Class = "Ultra"
         };
 
         var session7 = new Session
         {
-            CinemaHall = cinemaHall1,
-            Date = "12.12.2012",
-            Time = "21:12",
-            MovieName = "A Knives Out Mystery: Glass Onion",
-            SessionDuration = 132
+            CinemaHall = cinemaHall7,
+            Date = "11.11.2013",
+            Time = "11:11",
+            MovieName = "Astral",
+            SessionDuration = 112
         };
 
         var order7 = new Order
         {
-            Price = 12000,
-            Session = session1,
-            User = user1,
+            Price = 14000,
+            Session = session7,
+            User = user7,
             PaymentStatus = true
         };
 
@@ -255,106 +255,107 @@ public static class Seed
         
         var user8 = new User
         {
-            CardCredentials = "1234 5678 9898 7654", 
-            FullName = "Olimzhanov Umid", 
-            Email = "uolimzhanov@gmail.com",
+            CardCredentials = "1234 8765 9898 7654", 
+            FullName = "Nuraliev Umid", 
+            Email = "nuraliev@gmail.com",
             PhoneNumber = "+998901234567"
         };
 
         var cinemaHall8 = new CinemaHall
         {
             NumberOfSeats = 2,
-            Class = "Premium"
+            Class = "S"
         };
 
         var session8 = new Session
         {
-            CinemaHall = cinemaHall1,
+            CinemaHall = cinemaHall8,
             Date = "12.12.2012",
             Time = "21:12",
-            MovieName = "A Knives Out Mystery: Glass Onion",
+            MovieName = "The boys: The movie",
             SessionDuration = 132
         };
 
         var order8 = new Order
         {
-            Price = 12000,
-            Session = session1,
-            User = user1,
+            Price = 22000,
+            Session = session8,
+            User = user8,
             PaymentStatus = true
         };
 
         #endregion
 
         #region 9
-
+        
         var user9 = new User
         {
             CardCredentials = "1234 5678 9898 7654", 
-            FullName = "Olimzhanov Umid", 
-            Email = "uolimzhanov@gmail.com",
+            FullName = "Pulatxojaev Akbarxoja", 
+            Email = "pulatxojaev@gmail.com",
             PhoneNumber = "+998901234567"
         };
 
         var cinemaHall9 = new CinemaHall
         {
             NumberOfSeats = 2,
-            Class = "Premium"
+            Class = "BussinesMas"
         };
 
         var session9 = new Session
         {
-            CinemaHall = cinemaHall1,
-            Date = "12.12.2012",
+            CinemaHall = cinemaHall9,
+            Date = "12.12.2014",
             Time = "21:12",
-            MovieName = "A Knives Out Mystery: Glass Onion",
-            SessionDuration = 132
+            MovieName = "Free Guy",
+            SessionDuration = 142
         };
 
         var order9 = new Order
         {
-            Price = 12000,
-            Session = session1,
-            User = user1,
-            PaymentStatus = true
+            Price = 300,
+            Session = session9,
+            User = user9,
+            PaymentStatus = false
         };
-        
+
         #endregion
-        
+
         #region 10
-        
+
         var user10 = new User
         {
-            CardCredentials = "1234 5678 9898 7654", 
-            FullName = "Olimzhanov Umid", 
-            Email = "uolimzhanov@gmail.com",
+            CardCredentials = "1234 5678 8989 7654", 
+            FullName = "Alimov Shukrullo", 
+            Email = "alimov@gmail.com",
             PhoneNumber = "+998901234567"
         };
 
         var cinemaHall10 = new CinemaHall
         {
             NumberOfSeats = 2,
-            Class = "Premium"
+            Class = "Est deshevle"
         };
 
         var session10 = new Session
         {
-            CinemaHall = cinemaHall1,
+            CinemaHall = cinemaHall10,
             Date = "12.12.2012",
             Time = "21:12",
-            MovieName = "A Knives Out Mystery: Glass Onion",
+            MovieName = "Fast & Furious",
             SessionDuration = 132
         };
 
         var order10 = new Order
         {
-            Price = 12000,
-            Session = session1,
-            User = user1,
-            PaymentStatus = true
+            Price = 100,
+            Session = session10,
+            User = user10,
+            PaymentStatus = false
         };
-
+        
         #endregion
+
 
         #region add and save
 
